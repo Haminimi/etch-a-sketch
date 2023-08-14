fef378a97f04f13998802b0aa9fb8959f5048f76
Hamza Mujinovic <mujinovichamza@gmail.com>
2023-08-14 15:30:31 +0200

    Add grid setup and clear functionality
    The `setUpGrid` function adjusts the grid layout based on the specified
    size and creates grid elements with mouseover and click event listeners
    to change the color. The `clearGrid` function clears the grid by
    resetting its inner HTML. The `setCurrentMode` function allows setting
    the current drawing mode.

--- script.js
+++ script.js
@@ -50,3 +50,24 @@ function reloadGrid() {
     clearGrid()
     setUpGrid(currentSize)
 }
+
+function clearGrid() {
+    grid.innerHTML = '';
+}
+
+function setUpGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div')
+        gridElement.classList.add('grid-element')
+        gridElement.addEventListener('mouseover', changeColor)
+        gridElement.addEventListener('click', changeColor)
+        grid.appendChild(gridElement)
+    }
+}
+
+function setCurrentMode(newMode) {
+    currentMode = newMode
+}
